2d57abaf1e8faca2179a461f68d71c4c322260f9
Camilo Borges <camilob@gmail.com>
2016-10-20 15:26:28 +1100

    updated doco, little bit of refactoring scripts

---
+++ scripts/New-AppSecret.ps1
@@ -0,0 +1,7 @@
+﻿$length=44;
+$ascii=$NULL;
+For ($a=48;$a -le 122;$a++) {$ascii+=,[char][byte]$a }
+For ($loop=1; $loop -le $length; $loop++) {
+    $TempPassword+=($ascii | GET-RANDOM)
+}
+return $TempPassword
